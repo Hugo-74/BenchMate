1d27aa6d2955da30a79d85e3795263ceb6d417e8
Hugo <154477470+Hugo-74@users.noreply.github.com>
2026-03-11 19:48:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,48 +1,30 @@
-const CACHE_NAME = 'benchmate-cache-v1';
-
-// Liste des fichiers à sauvegarder pour le mode hors-ligne
+const CACHE_NAME = 'benchmate-v4';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Installation : on met en cache les fichiers de base
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Fichiers mis en cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Interception des requêtes : on sert le cache si disponible, sinon on va sur le réseau
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retourne la version en cache si elle existe, sinon télécharge la nouvelle
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Nettoyage des anciens caches lors des mises à jour
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
